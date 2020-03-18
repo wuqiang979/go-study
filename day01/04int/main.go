f95ed741d4f17f575744e9dc1eed6c144dd972aa
wuqiang <happyboy_tu@163.com>
2020-03-19 00:44:49 +0800

    整型

---
+++ day01/04int/main.go
@@ -0,0 +1,21 @@
+/*
+ * @Author: 尼大人
+ * @Date: 2020-03-19 00:05:50
+ * @LastEditors: 尼大人
+ * @LastEditTime: 2020-03-19 00:41:00
+ */
+package main
+
+import "fmt"
+
+// 整型
+
+func main() {
+	var i1 = 101
+	fmt.Printf("%d\n", i1)
+
+	// var i2 = 100
+	// fmt.Printf("i2:%s", i2)
+
+	fmt.Println(1 << 10)
+}
